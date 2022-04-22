07c07687171ad36ebee8ec1ed7dd31c1d73001a6
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2022-04-21 20:56:44 -0400

    Add FlexStrCore and regular impl block with from_static to code gen

--- generate/src/impls.rs
+++ generate/src/impls.rs
@@ -3,30 +3,30 @@ use flexgen::{import_vars, CodeFragment, Error};
 use flexstr::local_fmt;
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use quote_doctest::doc_comment;
+use quote_doctest::{doc_comment, doc_test};
 
-pub(crate) struct StrUse;
-
-impl CodeFragment for StrUse {
-    fn uses(&self, vars: &TokenVars) -> Result<TokenStream, Error> {
-        import_vars! { vars => suffix }
-
-        Ok(match suffix {
-            TokenValue::String(s) if s == "BStr" => quote! { use bstr::BStr; },
-            TokenValue::String(s) if s == "CStr" => quote! { use std::ffi::CStr; },
-            TokenValue::String(s) if s == "OsStr" => quote! { use std::ffi::OsStr; },
-            TokenValue::String(s) if s == "Path" => quote! { use std::path::Path; },
-            _ => quote! {},
-        })
+fn str_type_use(suffix: &TokenValue) -> TokenStream {
+    match suffix {
+        TokenValue::String(s) if s == "BStr" => quote! { use bstr::BStr; },
+        TokenValue::String(s) if s == "CStr" => quote! { use std::ffi::CStr; },
+        TokenValue::String(s) if s == "OsStr" => quote! { use std::ffi::OsStr; },
+        TokenValue::String(s) if s == "Path" => quote! { use std::path::Path; },
+        _ => quote! {},
     }
 }
 
 pub(crate) struct FlexStruct;
 
 impl CodeFragment for FlexStruct {
-    fn uses(&self, _vars: &TokenVars) -> Result<TokenStream, Error> {
+    fn uses(&self, vars: &TokenVars) -> Result<TokenStream, Error> {
+        import_vars! { vars => suffix }
+
+        let str_type_use = str_type_use(suffix);
+
         Ok(quote! {
+            #str_type_use
             use crate::FlexStrInner;
+            use crate::traits::FlexStrCore;
             use crate::traits::private::FlexStrCoreInner;
         })
     }
@@ -103,6 +103,86 @@ impl CodeFragment for FlexStruct {
                     &self.0
                 }
             }
+
+            _blank_!();
+            _comment_!("### FlexStrCore ###\n");
+            _blank_!();
+            impl<const SIZE: usize, const BPAD: usize, const HPAD: usize, HEAP>
+                FlexStrCore<'str, SIZE, BPAD, HPAD, HEAP, #str_type> for #ident<SIZE, BPAD, HPAD, HEAP>
+            where
+                HEAP: Storage<#str_type>,
+            {
+                #[inline(always)]
+                fn as_str_type(&self) -> &#str_type {
+                    self.inner().as_str_type()
+                }
+            }
+        })
+    }
+}
+
+struct FromStatic;
+
+impl CodeFragment for FromStatic {
+    fn uses(&self, vars: &TokenVars) -> Result<TokenStream, Error> {
+        import_vars! { vars => suffix }
+
+        let str_type_use = str_type_use(suffix);
+
+        Ok(quote! {
+            #str_type_use
+            use crate::FlexStrInner;
+        })
+    }
+
+    fn generate(&self, vars: &TokenVars) -> Result<TokenStream, Error> {
+        import_vars! { vars => suffix, str_type }
+
+        let local_ident = format_ident!("Local{suffix}");
+
+        let doc_test = doc_test!(quote! {
+            use flexstr::#local_ident;
+            _blank_!();
+
+            const S: #local_ident = #local_ident::from_static("test");
+            assert!(S.is_static());
+        })?;
+
+        Ok(quote! {
+            /// Creates a wrapped static string literal. This function is equivalent to using the macro and
+            /// is `const fn` so it can be used to initialize a constant at compile time with zero runtime cost.
+            #doc_test
+            #[inline(always)]
+            pub const fn from_static(s: &'static #str_type) -> Self {
+                Self(FlexStrInner::from_static(s))
+            }
+        })
+    }
+}
+
+pub(crate) struct FlexImpls;
+
+impl CodeFragment for FlexImpls {
+    fn uses(&self, vars: &TokenVars) -> Result<TokenStream, Error> {
+        let from_static_uses = FromStatic.uses(vars)?;
+
+        Ok(quote! {
+            #from_static_uses
+        })
+    }
+
+    fn generate(&self, vars: &TokenVars) -> Result<TokenStream, Error> {
+        import_vars! { vars => suffix }
+
+        let ident = format_ident!("Flex{suffix}");
+
+        let from_static = FromStatic.generate(vars)?;
+
+        Ok(quote! {
+            impl<'str, const SIZE: usize, const BPAD: usize, const HPAD: usize, HEAP>
+                #ident<'str, SIZE, BPAD, HPAD, HEAP> {
+                    #from_static
+                }
         })
     }
 }
--- generate/src/main.rs
+++ generate/src/main.rs
@@ -8,7 +8,7 @@ use flexgen::config::Config;
 use flexgen::{register_fragments, CodeGenerator, Error};
 
 fn main() -> Result<(), Error> {
-    let fragments = register_fragments!(StrUse, FlexStruct, TypeAliases);
+    let fragments = register_fragments!(FlexStruct, FlexImpls, TypeAliases);
     let config = Config::from_default_toml_file()?;
 
     let gen = CodeGenerator::new(fragments, config)?;
