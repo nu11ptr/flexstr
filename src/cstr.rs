5cf42b3a9841598bca8c71995456cddbd7769204
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2025-12-13 19:53:11 -0500

    Add basic CStr support

--- src/cstr.rs
+++ src/cstr.rs
@@ -1,7 +1,55 @@
-use alloc::{rc::Rc, sync::Arc};
+use alloc::{ffi::CString, rc::Rc, sync::Arc};
 use core::ffi::CStr;
 
-use crate::Flex;
+use crate::{Flex, RefCounted, StringOps};
 
 pub type LocalCStr<'s> = Flex<'s, CStr, Rc<CStr>>;
 pub type SharedCStr<'s> = Flex<'s, CStr, Arc<CStr>>;
+
+// NOTE: This one is a bit different because CString is just a Box<[u8]>. Instead of equal size,
+// we should be at most one machine word larger.
+const _: () = assert!(
+    size_of::<Option<LocalCStr>>() <= size_of::<CString>() + size_of::<usize>(),
+    "Option<LocalCStr> must be less than or equal to the size of CString plus one machine word"
+);
+const _: () = assert!(
+    size_of::<Option<SharedCStr>>() <= size_of::<CString>() + size_of::<usize>(),
+    "Option<SharedCStr> must be less than or equal to the size of CString plus one machine word"
+);
+
+impl<R: RefCounted<CStr>> Flex<'_, CStr, R> {
+    pub fn as_cstr(&self) -> &CStr {
+        self.as_borrowed_type()
+    }
+}
+
+impl StringOps for CStr {
+    #[cfg(feature = "safe")]
+    #[inline(always)]
+    fn bytes_as_self(bytes: &[u8]) -> &Self {
+        // PANIC SAFETY: We know the bytes are a valid CStr
+        CStr::from_bytes_with_nul(bytes).expect("Missing NUL byte")
+    }
+
+    #[cfg(not(feature = "safe"))]
+    #[inline(always)]
+    fn bytes_as_self(bytes: &[u8]) -> &Self {
+        // SAFETY: We know the bytes are a valid CStr
+        unsafe { CStr::from_bytes_with_nul_unchecked(bytes) }
+    }
+
+    #[inline(always)]
+    fn self_as_bytes(&self) -> &[u8] {
+        self.to_bytes_with_nul()
+    }
+}
+
+// *** From<CString> ***
+
+// NOTE: Cannot be implemented generically because of impl<T> From<T> for T
+impl<'s, R: RefCounted<CStr>> From<CString> for Flex<'s, CStr, R> {
+    #[inline(always)]
+    fn from(s: CString) -> Self {
+        Flex::from_owned(s)
+    }
+}
